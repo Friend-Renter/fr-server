098cf4b3505d2081485a019ff82d862cf823e7ca
Omar4589 <omar4589@hotmail.com>
2025-08-30 20:47:10 -0500

    chore(c0): scaffold server toolchain (TS, ESM, ESLint, Prettier, Jest, Husky)

---
+++ server/jest.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  preset: "ts-jest/presets/default-esm",
+  testEnvironment: "node",
+  extensionsToTreatAsEsm: [".ts"],
+  transform: {
+    "^.+\\.ts$": [
+      "ts-jest",
+      {
+        useESM: true,
+        tsconfig: "tsconfig.json"
+      }
+    ]
+  },
+  moduleFileExtensions: ["ts", "js", "json"],
+  testMatch: ["**/?(*.)+(spec|test).ts"],
+  coveragePathIgnorePatterns: ["/node_modules/", "/dist/"]
+};
